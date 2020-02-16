edc07a67664b01616429739a020434f0a24b394e
Nathaniel Foster <nathanielfoster@Nathaniels-MacBook-Pro.local>
2020-02-15 19:08:14 -0500

    update DisplayAsterisks

--- Module 3/Assignment/src/DisplayAsterisks.java
+++ Module 3/Assignment/src/DisplayAsterisks.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
 
-public class DisplayAsterisks {
+public class DisplayAsterisks 
+{
+	private static final int CHOICE_1 = 1;
+	private static final int CHOICE_2 = 2;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		boolean validResponse = false;
+
+		while(!validResponse)
+		{
+			validResponse = printAsterisks()
+		}
+
+	}
+
+	public static boolean printAsterisks(int asterisks, short choice)
+	{
+		boolean validChoice = false;
+
+		switch choice
+		{
+			case CHOICE_1:
+				validChoice = true;
+				break;
+			case CHOICE_2:
+				validChoice = true;
+				break;
+			default
+				break;
+		}
+
+		return validChoice;
 
 	}
 
