640ddbe75fb0f4ca74b9c3835e194723d7649d31
nfoster10 <nathanielfoster10@gmail.com>
2020-03-07 00:34:11 -0500

    comments for module 6 assignment

--- Module_6/src/Date.java
+++ Module_6/src/Date.java
@@ -1,25 +1,29 @@
-public class Date
+public class Date //defines an object of type Date
 {
 	private int month;
 	private int day;
 	private int year;
 
-	Date()
+	Date() //class concatenator
 	{
 		month = 0;
 		day = 0;
 		year = 0;
 	}
 
+	//Date::toString() returns a string variable of MM/DD/YYYY
 	public String toString()
 	{
 		return String.format("%2d/%2d/%4d", month, day, year);
 	}
 
+	//Date::setMonth() returns true and stores inputMonth if valid inputMonth (1 <= inputMonth <= 12)
+	//returns false and terminates processing if invalid inputMonth
 	public boolean setMonth(int inputMonth)
 	{
 		boolean isDataValid = true;
 
+		//check if valid inputMonth
 		if (inputMonth >= 1 && inputMonth <= 12)
 			this.month = inputMonth;
 		else
@@ -27,31 +31,40 @@ public class Date
 
 		return isDataValid;
 	}
+	//Date::getMonth() returns int variable month for the calling Date object
 	public int getMonth()
 	{
 		return this.month;
 	}
 
+	//Date::setDay() returns true and stores inputDay if input valid inputDay
+	//returns false and terminates processing if input invalid inputDay
+	//inputDay valid if greater than 0 AND less than or equal getDaysInMonth 
 	public boolean setDay(int inputDay)
 	{
 		boolean isDataValid = true;
 
-		if (inputDay <= this.getDaysInMonth())
+		if (inputDay <= this.getDaysInMonth() && inputDay > 0)
 			this.day = inputDay;
 		else
 			isDataValid = false;
 
 		return isDataValid;
 	}
+
+	//Date::getDay() returns int variable day for calling Date object
 	public int getDay()
 	{
 		return this.day;
 	}
 
+	//Date::setYear() returns true and stores inputYear if valid inputYear
+	//returns false and terminates processing if inputYear invalid
 	public boolean setYear(int inputYear)
 	{
 		boolean isDataValid = true;
 
+		//inputYear valid if between 1900 and 2020 inclusive
 		if(inputYear >= 1900 && inputYear <= 2020)
 			this.year = inputYear;
 		else
@@ -59,6 +72,8 @@ public class Date
 
 		return isDataValid;
 	}
+
+	//Date::getyear() returns int variable year for calling Date object
 	public int getYear()
 	{
 		return this.year;
@@ -66,7 +81,7 @@ public class Date
 
   /*
   PrintMonth::getDaysInMonth() returns integer value daysInMonth which is the number of days in the month 
-  for an input month 'm' and year 'y'
+  for object month and year
   Pre-conditions: m is 1-12, y is greater than 0
   Post-conditons: daysInMonth returned
   */
