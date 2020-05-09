c7f438c6672666d14f44be426ac965e2e1ea05ad
nfoster10 <nathanielfoster10@gmail.com>
2020-05-09 15:05:56 -0400

    touch up mini project 3

--- MiniProject3/src/LoanCalculator.java
+++ MiniProject3/src/LoanCalculator.java
@@ -30,30 +30,33 @@ public class LoanCalculator extends Application
 
 		Label annualInteretRateLabel = new Label ("Annual Interest Rate:  "); // create a label
 		annualInteretRateText = new TextField();
+		annualInteretRateText.setPromptText("(percent)");
 		rootNode.add( annualInteretRateLabel, 0, 0);
-		rootNode.add( annualInteretRateText, 1, 0); // add the label to the scene graph
+		rootNode.add( annualInteretRateText, 1, 0); 
 
 		Label numberOfYearsLabel = new Label("Number of Years: "); // create a label
 		numberOfYearsText = new TextField();
+		numberOfYearsText.setPromptText("(years)");
 		rootNode.add( numberOfYearsLabel, 0, 1);
-		rootNode.add( numberOfYearsText, 1, 1); // add the label to the scene graph
+		rootNode.add( numberOfYearsText, 1, 1); 
 
 		Label loanAmountLabel = new Label("Loan Amount: "); // create a label
 		loanAmountText = new TextField();
+		loanAmountText.setPromptText("(dollars)");
 		rootNode.add( loanAmountLabel, 0, 2);
-		rootNode.add( loanAmountText, 1, 2); // add the label to the scene graph
+		rootNode.add( loanAmountText, 1, 2); 
 
 		Label monthlyPaymentLabel = new Label("Monthly Payment: ");
 		monthlyPaymentText = new TextField("");
 		monthlyPaymentText.setEditable(false);
 		rootNode.add( monthlyPaymentLabel, 0, 3);
-		rootNode.add( monthlyPaymentText, 1, 3); // add the label to the scene graph
+		rootNode.add( monthlyPaymentText, 1, 3);
 
-		Label totalPaymentLabel = new Label      ("Total Payment: ");
+		Label totalPaymentLabel = new Label("Total Payment: ");
 		totalPaymentText = new TextField("");
 		totalPaymentText.setEditable(false);
 		rootNode.add( totalPaymentLabel, 0, 4);
-		rootNode.add( totalPaymentText, 1, 4); // add the label to the scene graph
+		rootNode.add( totalPaymentText, 1, 4);
 
 		Button calculateButton = new Button("Calculate");
 		rootNode.add(calculateButton, 1, 5);
@@ -65,6 +68,7 @@ public class LoanCalculator extends Application
 		loanCalculatorGUI.show(); // show the stage & its scene
 	}
 
+	//Action Event Handler for Calculate Button for Loan Calculator GUI
 	private class LoanCalculatorCalculateButtonHandler implements EventHandler<ActionEvent>
 	{
 		public void handle(ActionEvent buttonPress)
@@ -74,14 +78,14 @@ public class LoanCalculator extends Application
 
 			try {
 				double annualInteretRate = Double.parseDouble(annualInteretRateText.getText());
-				annualInteretRate = annualInteretRate > 1 ? annualInteretRate / 100 : annualInteretRate;
+				annualInteretRate = annualInteretRate / 1200;
 				int numberOfYears = Integer.parseInt(numberOfYearsText.getText());
 				double loanAmount = Double.parseDouble(loanAmountText.getText());
 
 				monthlyPayment = (double) 0;
 				totalPayment = 0.0;
 
-				monthlyPayment = (annualInteretRate * loanAmount) / (1 - Math.pow((1+annualInteretRate), -(12*numberOfYears)));
+				monthlyPayment = (annualInteretRate * loanAmount) / (1 - Math.pow((1 + annualInteretRate), - (12 * numberOfYears)));
 				totalPayment = monthlyPayment * 12 * numberOfYears;
 			}
 			catch (NumberFormatException e){
